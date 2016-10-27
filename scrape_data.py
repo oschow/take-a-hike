4428ef09d4776e0fe2d34fe953dcb855238c0dee
Olivia Schow <oliviaschow@Olivias-MacBook-Pro.local>
2016-10-27 09:20:44 -0600

    finished web scraping file

--- scrape_data.py
+++ scrape_data.py
@@ -11,23 +11,34 @@ def get_soup(url):
     return soup
 
 def parse_hikes(hike_soup, area):
-    title = hike_soup.find('div', id='trail-title')
-    name = title[0].findChild('h1').text
+    page_body = hike_soup.find('body')
+    title = page_body.select('div.trail-title')
+    name = title[0].findChild('h1').text.split('-')[0]
+    name = name.replace('.', '')
     hike_data_table = hike_soup.find('table', id='trail-details-table')
-    hike_area = area
-    round_trip_length = hike_data_table.findChildren('td')[1].text
-    start_end_elevation = hike_data_table.findChildren('td')[3].text
-    elevation_change = hike_data_table.findChildren('td')[5].text
+    hike_area = area.split('/')[-1]
+    round_trip_length = float(hike_data_table.findChildren('td')[1].text.split()[0])
+    start_elevation = hike_data_table.findChildren('td')[3].text.split()[0]
+    end_elevation = hike_data_table.findChildren('td')[3].text.split()[2]
+    net_elevation_gain = hike_data_table.findChildren('td')[5].text.split()[0]
     skill_level = hike_data_table.findChildren('td')[7].text
-    dogs_allowed = hike_data_table.findChildren('td')[9].text
-    gps = hike_soup.find('div', id='trail-description-gps-coordinates')
-    gps_coordinates = gps.findChildren('li')[0].text
-    desc = hike_soup.find('div', id='trail-description-description')
-    description = desc.findChildren('p')[1].text + desc.findChildren('p')[3:]
-    return {name: {'part_of_co': area,
+    dogs_allowed = bool(hike_data_table.findChildren('td')[9].text)
+    gps = hike_soup.select('div.trail-description-gps-coordinates')
+    if len(gps) != 0:
+        gps_coordinates = gps[0].findChildren('li')[0].text
+    else:
+        gps_coordinates = 'Null'
+    desc = hike_soup.select('div.trail-description-description')[0]
+    description = desc.findChildren('p')[0].text
+    if len(desc) > 1:
+        for text_chunk in desc.findChildren('p')[1:]:
+            text = ''.join(text_chunk.text)
+            description += text
+    return {name: {'part_of_co': hike_area,
                      'round_trip_length': round_trip_length,
-                     'start_end_elevation': start_end_elevation,
-                     'elevation_change': elevation_change,
+                     'start_elevation': start_elevation,
+                     'end_elevation': end_elevation,
+                     'net_elevation_gain': net_elevation_gain,
                      'skill_level': skill_level,
                      'dogs_allowed': dogs_allowed,
                      'gps_coordinates': gps_coordinates,
@@ -36,7 +47,7 @@ def parse_hikes(hike_soup, area):
 def get_hike_data(soup, area):
     hikes = soup.findAll('div', id='quicktabs-tabpage-pro_area_tabs-0')[0].findChildren('a')
     for hike in hikes:
-        hike_url = 'http://www.protrails.com/' + hike['href']
+        hike_url = 'http://www.protrails.com' + hike['href']
         hike_soup = get_soup(hike_url)
         mongo_doc = parse_hikes(hike_soup, area)
         table.insert_one(mongo_doc)
@@ -54,21 +65,12 @@ if __name__ == '__main__':
     rmnp_url = 'http://www.protrails.com/area/8/rocky-mountain-national-park'
     summit_eagle_clearcreek_url = 'http://www.protrails.com/area/41/summit-county-eagle-county-clear-creek-county'
 
-    # urls = [aspen_url, denver_url, national_monument_url, gsdnp_url, indian_peaks_url, rmnp_url, summit_eagle_clearcreek_url]
-    # for area in urls:
-    #     soup = get_soup(area)
-    #     get_hike_data(soup, area)
+    urls = [aspen_url, denver_url, national_monument_url, gsdnp_url, indian_peaks_url, rmnp_url, summit_eagle_clearcreek_url]
+    for area in urls:
+        soup = get_soup(area)
+        get_hike_data(soup, area)
 
-
-    aspen_soup = get_soup(aspen_url)
-    # denver_soup = get_soup(denver_url)
-    # national_monument_soup = get_soup(national_monument_url)
-    # gsdnp_soup = get_soup(gsdnp_url)
-    # indian_peaks_soup = get_soup(indian_peaks_url)
-    # rmnp_soup = get_soup(rmnp_url)
-    # summit_eagle_clearcreek_soup = get_soup(summit_eagle_clearcreek_url)
-
-    # c = table.find()
-    # for i in range(5):
-    #     print next(c)
-    #     raw_input('')
+    c = table.find()
+    for i in range(5):
+        print next(c)
+        raw_input('')
