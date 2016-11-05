b5b3e75857fdf790400b225d606e9fe187bf4e9b
Olivia Schow <oliviaschow@Olivias-MacBook-Pro.local>
2016-11-04 20:48:33 -0600

    updates web app

---
+++ web_app/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import graphlab as gl
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html', recs=None)
+
+
+@app.route('/choose-hike', methods=['GET', 'POST'])
+def enter_hike():
+	return render_template('choose-hike.html', recs=None)
+
+
+@app.route('/make-recommendations', methods=['POST', 'GET'])
+def get_recommendations():
+	hike = str(request.form.get('user_input'))
+	recs = model.recommend_from_interactions([hike], k=5)
+	return render_template('make-recommendations.html', recs=recs)
+
+
+if __name__ == '__main__':
+	model = gl.load_model('content_recommender')
+	app.run(host='0.0.0.0', port=8080, debug=True)
