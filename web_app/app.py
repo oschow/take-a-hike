c4d4fa2eed605365215a4ca1eb8a46b4b9848e26
Olivia Schow <oliviaschow@Olivias-MacBook-Pro.local>
2016-11-07 20:54:26 -0700

    update web app

--- web_app/app.py
+++ web_app/app.py
@@ -4,6 +4,15 @@ import graphlab as gl
 
 app = Flask(__name__)
 
+def get_hike_info(recs):
+	sf = gl.SFrame('../data/final.csv')
+	hike_info = []
+	for rec in recs:
+		hike = rec['hike_name']
+		info = sf[sf['hike_name']==hike]
+		hike_info.append(info)
+	return hike_info
+
 
 @app.route('/')
 def index():
@@ -19,9 +28,10 @@ def enter_hike():
 def get_recommendations():
 	hike = str(request.form.get('user_input'))
 	recs = model.recommend_from_interactions([hike], k=5)
-	return render_template('make-recommendations.html', recs=recs)
+	hike_data = get_hike_info(recs)
+	return render_template('make-recommendations.html', hike_data=hike_data)
 
 
 if __name__ == '__main__':
 	model = gl.load_model('content_recommender')
-	app.run(host='0.0.0.0', port=6969, debug=True)
+	app.run(host='0.0.0.0', port=7070, debug=True)
