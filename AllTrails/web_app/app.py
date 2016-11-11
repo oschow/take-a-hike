f500122e692c4ffee0c8c99b072dbc11be023908
Olivia Schow <oliviaschow@Olivias-MacBook-Pro.local>
2016-11-10 19:13:39 -0700

    update new web app with new models on all trails data

--- AllTrails/web_app/app.py
+++ AllTrails/web_app/app.py
@@ -4,32 +4,25 @@ import graphlab as gl
 
 app = Flask(__name__)
 
-def clean_df(df):
-	df['area_of_co'] = df['area_of_co'].map({'Aspen-Snowmass': 'Aspen/Snowmass', 'boulder-denver-golden-fort-collins-lyons': 'Denver/Boulder/Golden', 'colorado-national-monument': 'CO National Monument', 'great-sand-dunes-national-park': 'Great Sand Dunes National Park', 'indian-peaks-wilderness-area-james-peak-wilderness-area': 'Indian Peaks/James Peak Wilderness Area', 'rocky-mountain-national-park': 'Rocky Mountain National Park', 'summit-county-eagle-county-clear-creek-county': 'Summit/Eagle/Clear Creek County'})
-	df['drive_time_from_denver'] = df['drive_time_from_denver'].apply(lambda x: x/60)
-	df['drive_time_from_denver'] = df['drive_time_from_denver'].map(lambda x: ("%.2f" % x))
-	df['hike_url'] = df['hike_url'].apply(lambda x: x.strip())
-	return df
-
-def list_hikes(sf):
+def list_hikes(sf_hikes):
 	hikes = []
-	for h in sf['hike_name']:
+	for h in sf_hikes['hike_name']:
 		hikes.append(h)
 	return hikes
 
-def list_regions(sf):
-	regions = sf['area_of_co'].unique()
+def list_regions(sf_hikes):
+	regions = sf_hikes['hike_region'].unique()
 	return regions
 
 def get_info(hike):
-	data = sf[sf['hike_name']==hike]
+	data = sf_hikes[sf_hikes['hike_name']==hike]
 	return data
 
 def get_hike_info(recs):
 	hike_info = []
 	for rec in recs:
 		hike = rec['hike_name']
-		info = sf[sf['hike_name']==hike]
+		info = sf_hikes[sf_hikes['hike_name']==hike]
 		hike_info.append(info)
 	return hike_info
 
@@ -41,8 +34,8 @@ def index():
 
 @app.route('/choose-hike', methods=['GET', 'POST'])
 def enter_hike():
-	hikes = list_hikes(sf)
-	regions = list_regions(sf)
+	hikes = list_hikes(sf_hikes)
+	regions = list_regions(sf_hikes)
 	return render_template('choose-hike.html', hikes=hikes, regions=regions)
 
 
@@ -53,15 +46,25 @@ def get_recommendations():
 	miles = request.form.get('num-miles')
 	elevation = request.form.get('elevation-gain')
 	dog = request.form.get('dog')
-	recs = model.recommend_from_interactions([hike], k=5)
+	recs = content_model.recommend_from_interactions([hike], k=5)
 	your_hike = get_info(hike)
 	hike_data = get_hike_info(recs)
 	return render_template('make-recommendations.html', your_hike=your_hike, hike_data=hike_data)
 
+@app.route('/popular-hikes', methods=['POST', 'GET'])
+def get_popular():
+	recs = sf_hikes[sf_hikes['stars']==5.0]
+	best_hikes = get_hike_info(recs)
+	return render_template('get-popular.html', best_hikes=best_hikes)
+
 
 if __name__ == '__main__':
-	df = pd.read_csv('../data/final_with_url.csv')
-	df = clean_df(df)
-	sf = gl.SFrame(df)
-	model = gl.load_model('content_recommender')
-	app.run(host='0.0.0.0', port=7070, debug=True)
+	sf_hikes = gl.SFrame('../data/hikes_data_with_hike_name.csv')
+	sf_hikes = sf_hikes.remove_column('hike_id')
+	sf_ratings = gl.SFrame('../data/ratings_matrix.csv')
+	hike_side_data = gl.SFrame('../data/hikes_data_with_hike_id.csv')
+
+	content_model = gl.load_model('hike_content_recommender')
+	popular_model = gl.load_model('hike_popularity_recommender')
+
+	app.run(host='0.0.0.0', port=1515, debug=True)
