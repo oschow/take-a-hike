762bdf24b8d987ceec71e93e4f08e621c9a1ebe4
Olivia Schow <oliviaschow@Olivias-MacBook-Pro.local>
2016-11-13 11:28:28 -0700

    Adds file for ranking factorization recommender

---
+++ AllTrails/ranking_factorization_model.py
@@ -0,0 +1,19 @@
+import graphlab as gl
+import pandas as pd
+import cPickle as pickle
+
+if __name__ == '__main__':
+    sf = gl.SFrame('data/ratings_matrix.csv')
+    hike_side_data = gl.SFrame('data/hikes_data_with_hike_name.csv')
+    with open('data/hike_ids.pkl') as f:
+        hike_ids = pickle.load(f)
+
+    model = gl.recommender.ranking_factorization_recommender.create(sf_train, user_id='variable', item_id='hike_id', target='value', item_data=hike_side_data, ranking_regularization=0, regularization=1e-10, linear_regularization=1e-8)
+
+    recs = model.get_similar_items(items=['hike25'], k=5)
+
+    similar_hikes = []
+    for rec in recs:
+        hike = rec['similar']
+        name = hike_ids[hike]
+        similar_hikes.append(name)
